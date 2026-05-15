d8d3c0d8807288dba05edb9b15e9b07f02617817
yasmin <yasmin20vida@gmail.com>
2026-05-14 22:28:50 -0300

    Update remover.js

--- remover.js
+++ remover.js
@@ -1,20 +1,12 @@
-// Função para remover uma tarefa da lista
-function removerTarefa(li) {
-  // Pega a lista de tarefas
-  const lista = document.getElementById("lista");
-  // Pega o contador total de tarefas
-  const contador = document.getElementById("contador");
 
-  // Remove a tarefa do HTML (a exclui)
+function removerTarefa(li) {
+  const lista = document.getElementById("lista");
+  const contador = document.getElementById("contador");
   li.remove();
-  // Atualiza o contador total de tarefas
   contador.textContent = lista.children.length;
   
-  // Pega o contador de tarefas concluídas
   const contadorConcluidas = document.getElementById("contador-concluidas");
-  // Procura todas as tarefas que têm a classe 'concluida' e conta quantas são
   const totalConcluidas = lista.querySelectorAll("span.concluida").length;
-  // Atualiza o contador de concluídas
   contadorConcluidas.textContent = totalConcluidas;
   salvanoLocalStorage();
 }
